2f86b001533d502dd5d59e56363c3110e0dea07f
Kewzy <kozlovskisarmands@inbox.lv>
2020-10-21 18:39:57 +0300

    :tada: React App

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Made by me, maintained by me and
+                supported also by me.
+            </p>
+        </React.Fragment>
+    )
+}
